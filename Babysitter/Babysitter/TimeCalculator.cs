149e40902a80afec5f744e667d533282c853a9d4
Brenden Sharp <becuzz04@yahoo.com>
2016-06-07 23:34:25 -0400

    Initial commit. Created first failing test to check calculation of hours for whole hours

---
+++ Babysitter/Babysitter/TimeCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Babysitter
+{
+    public static class TimeCalculator
+    {
+        public static int CalculateHours(DateTime startTime, DateTime endTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Babysitter/BabysitterTests/TimeCalculationTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Babysitter;
+using NUnit.Framework;
+
+namespace BabysitterTests
+{
+    [TestFixture]
+    public class TimeCalculationTests
+    {
+        [Test]
+        public void GivenAWholeNumberOfHoursReturnTheHours()
+        {
+            Assert.AreEqual(5, TimeCalculator.CalculateHours(DateTime.Parse("1/1/2016 12:00 PM"), DateTime.Parse("1/1/2016 5:00 PM")));
+        }
+    }
+}
